325eb9f6f5049e4883a6211f82872155e19e226e
Roger Spooner <roger.i.w.spooner@gmail.com>
2023-07-29 22:02:39 +0100

    Moved I2C to the recommended pins for the ESP32

--- main/i2c_gyro_main.c
+++ main/i2c_gyro_main.c
@@ -24,8 +24,8 @@
 
 static const char *TAG = "i2c-simple-example";
 
-#define I2C_MASTER_SCL_IO           19      /*!< GPIO number used for I2C master clock */
-#define I2C_MASTER_SDA_IO           18      /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_SCL_IO           22      /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO           21      /*!< GPIO number used for I2C master data  */
 #define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
 #define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
 #define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
